36dbd2d19caf1b2e39f08c2bf5dd796de0f1bd00
Stanislav Zakahrov <sv.zakharov.24@gmail.com>
2024-04-04 13:53:23 +0300

    debugging

--- src/test/java/helper/CapabilityHelper.java
+++ src/test/java/helper/CapabilityHelper.java
@@ -3,6 +3,8 @@ package helper;
 import config.ConfigReader;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.AutomationName;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class CapabilityHelper {
 
@@ -13,6 +15,26 @@ public class CapabilityHelper {
     private static final String APP = ConfigReader.emulatorConfig.app();
     public static UiAutomator2Options options = new UiAutomator2Options();
 
+
+    public DesiredCapabilities getDesiredCapabilitiesOptions() {
+        DesiredCapabilities options = new DesiredCapabilities();
+        String appiumChallengeApk = System.getProperty("user.dir") + APP;
+
+        DEVICE_NAME = System.getProperty("deviceName") != null ? System.getProperty("deviceName") : DEVICE_NAME;
+        PLATFORM_NAME = System.getProperty("platformName") != null ? System.getProperty("platformName") : PLATFORM_NAME;
+        APP_PACKAGE = System.getProperty("appPackage") != null ? System.getProperty("appPackage") : APP_PACKAGE;
+        APP_ACTIVITY = System.getProperty("appActivity") != null ? System.getProperty("appActivity") : APP_ACTIVITY;
+
+        options.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, true);
+        options.setCapability("deviceName", DEVICE_NAME);
+        options.setCapability("platformName", PLATFORM_NAME);
+        options.setCapability("appPackage", APP_PACKAGE);
+        options.setCapability("appActivity", APP_ACTIVITY);
+        options.setCapability("automationName", AutomationName.ANDROID_UIAUTOMATOR2);
+
+        options.setCapability("app", appiumChallengeApk);
+        return options;
+    }
 
     public UiAutomator2Options configureCapabilityOptions() {
         options = new UiAutomator2Options();
@@ -28,6 +50,7 @@ public class CapabilityHelper {
         options.setCapability("platformName", PLATFORM_NAME);
         options.setCapability("appPackage", APP_PACKAGE);
         options.setCapability("appActivity", APP_ACTIVITY);
+        options.setCapability("automationName", AutomationName.ANDROID_UIAUTOMATOR2);
 //        options.setCapability("automationName", "Flutter");
         options.setCapability("app", appiumChallengeApk);
         return options;
