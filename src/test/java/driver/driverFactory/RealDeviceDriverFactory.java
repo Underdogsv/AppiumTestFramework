3b33acaf9953145bc67399e43b6f4d13c5d7f59e
Stanislav Zakahrov <sv.zakharov.24@gmail.com>
2024-04-04 13:58:06 +0300

    debugging

--- src/test/java/driver/driverFactory/RealDeviceDriverFactory.java
+++ src/test/java/driver/driverFactory/RealDeviceDriverFactory.java
@@ -5,13 +5,15 @@ import helper.CapabilityHelper;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class RealDeviceDriverFactory implements AppiumDriverFactory {
     public AppiumDriver createDriver(){
         String platformName = ConfigReader.emulatorConfig.platformName();
         String deviceType = ConfigReader.emulatorConfig.deviceType();
+//        UiAutomator2Options options = new CapabilityHelper().configureCapabilityOptions();
+        DesiredCapabilities options = new CapabilityHelper().getDesiredCapabilitiesOptions();
         if (platformName.equalsIgnoreCase("android")){
-            UiAutomator2Options options = new CapabilityHelper().configureCapabilityOptions();
             return new AndroidDriver(options);
         }else if (platformName.equalsIgnoreCase("iOs")){
             //    returns IOs driver if it will be implemented
